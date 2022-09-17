976be6792c0148c9c29df5883b61032b13be5673
beccabeccaaa <beccaras1@gmail.com>
2022-09-17 15:44:50 -0600

    fix: change of basis function

--- src/basis.py
+++ src/basis.py
@@ -2,20 +2,21 @@ import unittest
 import numpy
 
 #Could call image, preimage, domain, etc.
-def changeOfBasis( b1, b2, x1 ):
+def changeOfBasis( b1, b2, x1 ): #b1 is the preimage basis, b2 is the image basis
     #Write code to pass test
-    x2 = numpy.array( [0.5, 0.5] ).T
-    T = 0
+    T = numpy.linalg.solve( b2, b1 ) #Multiply the inverse of b1 by b2
+    x2 = numpy.dot( T, x1 ) #Multiply T by x1
     return x2, T #T is our change of basis operator
 
 class Test_changeOfBasis( unittest.TestCase ):
     def test_standardR2BasisRotate( self ):
-        b1 = numpy.eye(2)
+        b1 = numpy.eye(2) #This is a 2x2 identity matrix
         b2 = numpy.array([ [0, 1], [-1, 0] ] ).T
         x1 = numpy.array( [0.5, 0.5] ).T
         x2, T = changeOfBasis( b1, b2, x1 )
         v1 = b1 @ x1 #@ is for vector-vector multiplication in numpy
         v2 = b2 @ x2
+        #By hand, I should determine what T should be, add a test for it, add correct assert (mayybe self.assertTrue(nump.allclose(hard coded T)))
         self.assertTrue( numpy.allclose( v1, v2 ) ) #allclose is a tolerance
 
     def test_standardR2BasisSkew( self ):
