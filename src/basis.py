c4d15f6fe8884cf705523f503345f8c9c3b655d7
beccabeccaaa <beccaras1@gmail.com>
2022-09-17 14:57:20 -0600

    test: add change of basis test

---
+++ src/basis.py
@@ -0,0 +1,19 @@
+class Test_changeOfBasis( unittest.TestCase ):
+    def test_standardR2BasisRotate( self ):
+        b1 = numpy.eye(2)
+        b2 = numpy.array([ [0, 1], [-1, 0] ] ).T
+        x1 = numpy.array( [0.5, 0.5] ).T
+        x2, T = changeOfBasis( b1, b2, x1 )
+        v1 = b1 @ x1
+        v2 = b2 @ x2
+        self.assertTrue( numpy.allclose( v1, v2 ) )
+
+    def test_standardR2BasisSkew( self ):
+        b1 = numpy.eye(2)
+        b2 = numpy.array([ [0, 1], [0.5, 0.5] ] ).T
+        x1 = numpy.array( [0.5, 0.5] ).T
+        x2, T = changeOfBasis( b1, b2, x1 )
+        v1 = b1 @ x1
+        v2 = b2 @ x2
+        self.assertTrue( numpy.allclose( x2, numpy.array( [0.0, 1.0] ) ) )
+        self.assertTrue( numpy.allclose( v1, v2 ) )
