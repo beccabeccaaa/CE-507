497da5c3a746a29786e52d7a00fc3cffc50f5c18
beccabeccaaa <beccaras1@gmail.com>
2022-09-17 20:25:56 -0600

    fix: edited function to work correctly

--- src/basis.py
+++ src/basis.py
@@ -1,23 +1,45 @@
 import unittest
 import numpy
 
-#Could call image, preimage, domain, etc.
-def changeOfBasis( b1, b2, x1 ): #b1 is the preimage basis, b2 is the image basis
-    #Write code to pass test
-    T = numpy.linalg.solve( b2, b1 ) #Multiply the inverse of b1 by b2
-    x2 = numpy.dot( T, x1 ) #Multiply T by x1
-    return x2, T #T is our change of basis operator
+#b1 (C, Image basis)
+#b2 (D, Pre-image basis)
+#x1 (c)
+#x2 (d)
+#v1 (Cc)
+#v2 (Dd)
+#T (Change of basis operator, = D^-1 * C)
+
+def changeOfBasis( b1, b2, x1 ):
+    #Multiply the inverse of b2 by b1 without really taking the inverse of b2
+    T = numpy.linalg.solve( b2, b1 ) #Row vectors
+    #Multiply T by x1
+    x2 = numpy.dot( T, x1 ) #Column vector
+    #x2 = T @ x1
+    return x2, T
 
 class Test_changeOfBasis( unittest.TestCase ):
     def test_standardR2BasisRotate( self ):
-        b1 = numpy.eye(2) #This is a 2x2 identity matrix
-        b2 = numpy.array([ [0, 1], [-1, 0] ] ).T
-        x1 = numpy.array( [0.5, 0.5] ).T
+        b1 = numpy.eye(2) #This is a 2x2 identity matrix, row vectors
+        b2 = numpy.array([ [0, 1], [-1, 0] ] ).T #Column vectors; WATCH OUT
+        x1 = numpy.array( [0.5, 0.5] ).T #Column vector
+
+        #b1 = numpy.array([ [1, 4], [3, 1] ]) #Row vectors
+        #b2 = numpy.array([ [17, 7], [7, 10] ]) #Row vectors
+        #x1 = numpy.array( [2, 5] ).T #Column vector
+
+        #b1 = numpy.array([ [1, 3], [4, 1] ]) #Row vectors
+        #b2 = numpy.array([ [1, 0], [0, 1] ]) #Row vectors
+        #x1 = numpy.array( [2, 5] ).T #Column vector
+
         x2, T = changeOfBasis( b1, b2, x1 )
         v1 = b1 @ x1 #@ is for vector-vector multiplication in numpy
         v2 = b2 @ x2
-        #By hand, I should determine what T should be, add a test for it, add correct assert (mayybe self.assertTrue(nump.allclose(hard coded T)))
+        #R = numpy.linalg.inv(b2) @ b1
+        #print( "T = ", T)
+        #print("x1 = ", x1)
+        #print("x2 = ", x2)
         self.assertTrue( numpy.allclose( v1, v2 ) ) #allclose is a tolerance
+        #self.assertTrue( numpy.allclose( T, R ) )
 
     def test_standardR2BasisSkew( self ):
         b1 = numpy.eye(2)
