34becc762c5e47e4832f9ebb9e6c6a3e065ff90c
beccabeccaaa <beccaras1@gmail.com>
2022-09-23 00:04:35 -0600

    Add: Comments to taylor_expansion function

--- src/taylor_expansion.py
+++ src/taylor_expansion.py
@@ -1,10 +1,14 @@
 import sympy
 
+#a is the value at which the expansion is centered
+#x is the variable that remains
+#t is the taylor expansion
+
 def taylorExpansion( fun, a, order ):
     x = list( fun.atoms( sympy.Symbol ) )[0]
     t = 0
     for i in range( 0, order + 1 ):
-        df = sympy.diff( fun, x, i )
+        dsf = sympy.diff( fun, x, i ) #Take derivative with variables (take the ith derivative of fun with respect to x) (Next line: and evaluate at a)
         term = ( df.subs( x, a ) / sympy.factorial( i ) ) * ( x - a )**i
         t += term
     return t
