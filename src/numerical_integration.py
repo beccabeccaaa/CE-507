3f81e320c5249703803fdd28b32fa65322908d44
beccabeccaaa <beccaras1@gmail.com>
2022-09-29 11:48:27 -0600

    Add: New file--not useful yet

---
+++ src/numerical_integration.py
@@ -0,0 +1,43 @@
+import unittest
+import math
+import numpy as np
+import sympy
+
+
+
+
+
+
+
+class Test_computeRiemannQuadrature( unittest.TestCase ):
+    def test_integrate_constant_one( self ):
+        constant_one = lambda x : 1
+        for num_points in range( 1, 100 ):
+            self.assertAlmostEqual( first = riemannQuadrature( fun = constant_one, num_points = num_points ), second = 2.0, delta = 1e-12 )
+
+    def test_integrate_linear( self ):
+        linear = lambda x : x
+        for num_points in range( 1, 100 ):
+            self.assertAlmostEqual( first = riemannQuadrature( fun = linear, num_points = num_points ), second = 0.0, delta = 1e-12 )
+
+    def test_integrate_quadratic( self ):
+        linear = lambda x : x**2
+        error = []
+        for num_points in range( 1, 100 ):
+            error.append( abs( (2.0 / 3.0) - riemannQuadrature( fun = linear, num_points = num_points ) ) )
+        self.assertTrue( numpy.all( numpy.diff( error ) <= 0.0 ) )
+
+    def test_integrate_sin( self ):
+        sin = lambda x : math.sin(x)
+        error = []
+        for num_points in range( 1, 100 ):
+            self.assertAlmostEqual( first = riemannQuadrature( fun = sin, num_points = num_points ), second = 0.0, delta = 1e-12 )
+
+    def test_integrate_cos( self ):
+        cos = lambda x : math.cos(x)
+        error = []
+        for num_points in range( 1, 100 ):
+            error.append( abs( (2.0 / 3.0) - riemannQuadrature( fun = cos, num_points = num_points ) ) )
+        self.assertTrue( numpy.all( numpy.diff( error ) <= 0.0 ) )
+
+Listing 23
