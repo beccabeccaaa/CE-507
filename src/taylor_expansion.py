58894b1ab9f13ad6367bc05423ccb5eeaa37a26f
beccabeccaaa <beccaras1@gmail.com>
2022-09-30 00:29:42 -0600

    Add: Functions for each plot required in the homework

--- src/taylor_expansion.py
+++ src/taylor_expansion.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 import sympy
 import scipy
 import numpy as np
@@ -18,48 +19,54 @@ def taylorExpansion(fun, a, order):
         print(t)
     return t
 
-def plot_taylor_sin():
-    x = np.linspace(-1, 1)
-    for degree in np.array([0, 1, 3, 5, 7]):
-        sin_taylor = scipy.interpolate.approximate_taylor_polynomial(np.sin, 0, degree, 1)
-        plt.plot(x, sin_taylor(np.pi * x))
-    plt.plot(x, np.sin(np.pi * x), color = "black")
+def plot(x, fun, domain, degreeValues, functionLabel):
+    numPoints = 1000 #Larger numPoints value refines curves
+    px = np.linspace(domain[0], domain[1], numPoints) #Splits interval from left bound to right bound into N equally spaced points--to be used for plotting x-values
+    py = np.zeros(numPoints) #Creates a vector of size N filled with zeros--to be used for plotting actual function's y-values
+    for degree in degreeValues: #Evaluate taylor expansion to create plot
+        t = taylorExpansion(fun, 0, degree) #y-values of taylor expansion
+        for i in range(0, numPoints):
+            py[i] = t.subs(x, px[i])
+        plt.plot(px, py, label = "T(" + functionLabel + ", degree " + str(degree) + ")") #Create the plot
+    for i in range(0, numPoints):
+        py[i] = fun.subs(x, px[i]) #Evaluate function to create plot
+    plt.plot(px, py, color = "black", label = functionLabel)
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.legend(loc = "upper left", prop = {"size":6})
+    #plt.legend(loc = 'upper center', bbox_to_anchor = (0.5, 1.15), ncol = 3, fancybox = True, shadow = True)
     plt.show()
 
-def plot_taylor_e():
-    x = np.linspace(-1, 1)
-    for degree in np.array([1, 2, 3, 4]):
-        exp_taylor = scipy.interpolate.approximate_taylor_polynomial(np.exp, 0, degree, 1)
-        plt.plot(x, exp_taylor(x))
-    plt.plot(x, np.exp(x), color = "black")
-    plt.show()
+def plotTaylorSin():
+    x = sympy.symbols('x')
+    fun = sympy.sin(sympy.pi * x) #Create symbolic function
+    domain = [-1, 1]
+    degreeValues = np.array([0, 1, 3, 5, 7])
+    functionLabel = "sin(x)"
+    plot(x, fun, domain, degreeValues, functionLabel)
 
-def plot_taylor_erfc():
-    x = np.linspace(-2, 2)
-    for degree in [1, 3, 5, 7]:
-        erfc_taylor = scipy.interpolate.approximate_taylor_polynomial(scipy.special.erfc, 0, degree, 1)
-        plt.plot(x, erfc_taylor(x))
-    plt.plot(x, scipy.special.erfc(x), color = "black")
-    plt.show()
+def plotTaylorE():
+    x = sympy.symbols('x')
+    fun = sympy.exp(x) #Create symbolic function
+    domain = [-1, 1]
+    degreeValues = np.array([0, 1, 2, 3, 4])
+    functionLabel = "e^x"
+    plot(x, fun, domain, degreeValues, functionLabel)
 
-def plot_monomial_basis():
+def plotTaylorErfc():
+    x = sympy.symbols('x')
+    fun = sympy.erfc(x) #Create symbolic function
+    domain = [-2, 2]
+    degreeValues = np.array([0, 1, 3, 5, 7, 9, 11])
+    functionLabel = "erfc(x)"
+    plot(x, fun, domain, degreeValues, functionLabel)
+
+def plotMonomialBasis(highDegree):
     x = np.linspace(0, 1)
-    for degree in range(0, 11):
+    for degree in range(0, highDegree):
         plt.plot(x, x**degree)
     plt.show()
 
-x = sympy.symbols('x')
-fun = sympy.sin(sympy.pi * x) #Create symbolic function
-numPoints = 1000
-px = np.linspace(-1, 1, numPoints) #Splits interval from -1 to 1 into N equally spaced points--to be used for plotting x-values
-py = np.zeros(numPoints) #Creates a vector of size N filled with zeros--to be used for plotting y-values
-for degree in np.array([0, 1, 3, 5, 7]): #Evaluate taylor expansion to create plot
-    t = taylorExpansion(fun, 0, degree) #y
-    for i in range(0, numPoints):
-        py[i] = t.subs(x, px[i])
-    plt.plot(px, py, label = "T(sin(x), degree " + str(degree) + ")") #Create the plot
-for i in range(0, numPoints):
-    py[i] = fun.subs(x, px[i]) #Evaluate function to create plot
-plt.plot(px, py, color = "black", label = "sin(x)")
-plt.legend(loc = "upper left")
-plt.show()
+plotTaylorSin()
+plotTaylorE()
+plotTaylorErfc()
