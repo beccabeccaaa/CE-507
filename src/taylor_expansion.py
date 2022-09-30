a481a4a7384a75bcb54cec2d4f89ff23d793a314
beccabeccaaa <beccaras1@gmail.com>
2022-09-29 23:31:26 -0600

    Add: Correct plotting for sin

--- src/taylor_expansion.py
+++ src/taylor_expansion.py
@@ -1,5 +1,6 @@
 import unittest
 import sympy
+import scipy
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,22 +18,48 @@ def taylorExpansion(fun, a, order):
         print(t)
     return t
 
-#Create symbolic function & taylor expansion
+def plot_taylor_sin():
+    x = np.linspace(-1, 1)
+    for degree in np.array([0, 1, 3, 5, 7]):
+        sin_taylor = scipy.interpolate.approximate_taylor_polynomial(np.sin, 0, degree, 1)
+        plt.plot(x, sin_taylor(np.pi * x))
+    plt.plot(x, np.sin(np.pi * x), color = "black")
+    plt.show()
+
+def plot_taylor_e():
+    x = np.linspace(-1, 1)
+    for degree in np.array([1, 2, 3, 4]):
+        exp_taylor = scipy.interpolate.approximate_taylor_polynomial(np.exp, 0, degree, 1)
+        plt.plot(x, exp_taylor(x))
+    plt.plot(x, np.exp(x), color = "black")
+    plt.show()
+
+def plot_taylor_erfc():
+    x = np.linspace(-2, 2)
+    for degree in [1, 3, 5, 7]:
+        erfc_taylor = scipy.interpolate.approximate_taylor_polynomial(scipy.special.erfc, 0, degree, 1)
+        plt.plot(x, erfc_taylor(x))
+    plt.plot(x, scipy.special.erfc(x), color = "black")
+    plt.show()
+
+def plot_monomial_basis():
+    x = np.linspace(0, 1)
+    for degree in range(0, 11):
+        plt.plot(x, x**degree)
+    plt.show()
+
 x = sympy.symbols('x')
-fun = sympy.sin(sympy.pi * x)
-t = taylorExpansion(fun, 0, 4) #y
-
-#Evaluate function and taylor expansion to create plot data
-N = 1000
-px = np.linspace(-1, 1, N)
-py = np.zeros(N)
-fy = np.zeros(N)
-for i in range(0, N ):
-    fy[i] = fun.subs(x, px[i])
-    py[i] = t.subs(x, px[i])
-
-#Create the plot
-fig, ax = plt.subplots()
-ax.plot(px, fy, linewidth=2.0)
-ax.plot(px, py, linewidth=2.0)
+fun = sympy.sin(sympy.pi * x) #Create symbolic function
+numPoints = 1000
+px = np.linspace(-1, 1, numPoints) #Splits interval from -1 to 1 into N equally spaced points--to be used for plotting x-values
+py = np.zeros(numPoints) #Creates a vector of size N filled with zeros--to be used for plotting y-values
+for degree in np.array([0, 1, 3, 5, 7]): #Evaluate taylor expansion to create plot
+    t = taylorExpansion(fun, 0, degree) #y
+    for i in range(0, numPoints):
+        py[i] = t.subs(x, px[i])
+    plt.plot(px, py, label = "T(sin(x), degree " + str(degree) + ")") #Create the plot
+for i in range(0, numPoints):
+    py[i] = fun.subs(x, px[i]) #Evaluate function to create plot
+plt.plot(px, py, color = "black", label = "sin(x)")
+plt.legend(loc = "upper left")
 plt.show()
