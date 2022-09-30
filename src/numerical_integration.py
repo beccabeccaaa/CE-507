a95ca63413d7a0425bd486055fa5965ce16d2694
beccabeccaaa <beccaras1@gmail.com>
2022-09-29 20:10:52 -0600

    Edited name of function that computes the riemann sum quadrature

--- src/numerical_integration.py
+++ src/numerical_integration.py
@@ -13,7 +13,7 @@ def getRiemannQuadrature(num_points):
     wQuadrature = np.diff(x[0::2]) #Take every second index starting from index 0
     return xQuadrature, wQuadrature
 
-def computeRiemannQuadrature(fun, num_points):
+def riemannQuadrature(fun, num_points):
     xQuadrature, wQuadrature = getRiemannQuadrature(num_points = num_points)
     integral = 0.0
     for i in range( 0, num_points ):
@@ -64,40 +64,6 @@ def getGaussLegendreQuadrature(num_points):
         raise( Exception( "num_points_MUST_BE_INTEGER_IN_[1-5]" ) )
     return x, w
 
-
-
-
-class Test_computeRiemannQuadrature( unittest.TestCase ):
-    def test_integrate_constant_one( self ):
-        constant_one = lambda x : 1
-        for num_points in range( 1, 100 ):
-            self.assertAlmostEqual( first = riemannQuadrature( fun = constant_one, num_points = num_points ), second = 2.0, delta = 1e-12 )
-
-    def test_integrate_linear( self ):
-        linear = lambda x : x
-        for num_points in range( 1, 100 ):
-            self.assertAlmostEqual( first = riemannQuadrature( fun = linear, num_points = num_points ), second = 0.0, delta = 1e-12 )
-
-    def test_integrate_quadratic( self ):
-        linear = lambda x : x**2
-        error = []
-        for num_points in range( 1, 100 ):
-            error.append( abs( (2.0 / 3.0) - riemannQuadrature( fun = linear, num_points = num_points ) ) )
-        self.assertTrue( np.all( np.diff( error ) <= 0.0 ) )
-
-    def test_integrate_sin( self ):
-        sin = lambda x : math.sin(x)
-        error = []
-        for num_points in range( 1, 100 ):
-            self.assertAlmostEqual( first = riemannQuadrature( fun = sin, num_points = num_points ), second = 0.0, delta = 1e-12 )
-
-    def test_integrate_cos( self ):
-        cos = lambda x : math.cos(x)
-        error = []
-        for num_points in range( 1, 100 ):
-            error.append( abs( (2.0 / 3.0) - riemannQuadrature( fun = cos, num_points = num_points ) ) )
-        self.assertTrue( np.all( np.diff( error ) <= 0.0 ) )
-
 class Test_getRiemannQuadrature( unittest.TestCase ):
     def test_zero_points( self ):
         with self.assertRaises( Exception ) as context:
@@ -133,3 +99,33 @@ class Test_getRiemannQuadrature( unittest.TestCase ):
             self.assertIsInstance( obj = x, cls = np.ndarray )
             self.assertIsInstance( obj = w, cls = np.ndarray )
 
+class Test_computeRiemannQuadrature( unittest.TestCase ):
+    def test_integrate_constant_one( self ):
+        constant_one = lambda x : 1
+        for num_points in range( 1, 100 ):
+            self.assertAlmostEqual( first = riemannQuadrature( fun = constant_one, num_points = num_points ), second = 2.0, delta = 1e-12 )
+
+    def test_integrate_linear( self ):
+        linear = lambda x : x
+        for num_points in range( 1, 100 ):
+            self.assertAlmostEqual( first = riemannQuadrature( fun = linear, num_points = num_points ), second = 0.0, delta = 1e-12 )
+
+    def test_integrate_quadratic( self ):
+        linear = lambda x : x**2
+        error = []
+        for num_points in range( 1, 100 ):
+            error.append( abs( (2.0 / 3.0) - riemannQuadrature( fun = linear, num_points = num_points ) ) )
+        self.assertTrue( np.all( np.diff( error ) <= 0.0 ) )
+
+    def test_integrate_sin( self ):
+        sin = lambda x : math.sin(x)
+        error = []
+        for num_points in range( 1, 100 ):
+            self.assertAlmostEqual( first = riemannQuadrature( fun = sin, num_points = num_points ), second = 0.0, delta = 1e-12 )
+
+    def test_integrate_cos( self ):
+        cos = lambda x : math.cos(x)
+        error = []
+        for num_points in range( 1, 100 ):
+            error.append( abs( (2.0 / 3.0) - riemannQuadrature( fun = cos, num_points = num_points ) ) )
+        self.assertTrue( np.all( np.diff( error ) <= 0.0 ) )
